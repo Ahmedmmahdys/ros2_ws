905b16c68871b112b04b7e8eccea328cc3ea3789
Ahmedmmahdys <168310994+Ahmedmmahdys@users.noreply.github.com>
2025-10-25 15:02:00 +0100

    Add stub control_msgs package and fix rcan_core install markers

--- src/rcan_core/setup.py
+++ src/rcan_core/setup.py
@@ -7,6 +7,10 @@ setup(
     version='0.1.0',
     packages=[package_name, f'{package_name}.api', f'{package_name}.services'],
     package_data={package_name: ['py.typed']},
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/rcan_core']),
+        ('share/' + package_name, ['package.xml']),
+    ],
     install_requires=['setuptools'],
     zip_safe=False,
     maintainer='RCAN Team',
