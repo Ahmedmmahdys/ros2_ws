3560d775ccf4175e6c8045b13985d3036974c552
Ahmedmmahdys <168310994+Ahmedmmahdys@users.noreply.github.com>
2025-10-25 17:44:35 +0100

    Silence catkin cache warnings and expose shim header

---
+++ src/moveit_improved_interface/env-hooks/moveit_improved_interface.bat
@@ -0,0 +1,4 @@
+@echo off
+rem generated from moveit_improved_interface/env-hooks/moveit_improved_interface.bat
+
+call ament_append_value.bat CPATH "%AMENT_CURRENT_PREFIX%\include"
---
+++ src/moveit_improved_interface/env-hooks/moveit_improved_interface.sh
@@ -0,0 +1,3 @@
+# generated from moveit_improved_interface/env-hooks/moveit_improved_interface.sh
+
+ament_append_value CPATH "$AMENT_CURRENT_PREFIX/include"
